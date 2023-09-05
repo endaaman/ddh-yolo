9d5c25229514b46a627b5bac4d09b3c353323724
endaaman <ken@endaaman.me>
2023-09-05 11:35:21 +0900

    use 6 folds

--- tasks.py
+++ tasks.py
@@ -2,9 +2,8 @@ import invoke
 
 @invoke.task
 def train_all(c):
-    for i in range(0, 5):
+    for i in range(6):
         num = i + 1
-
         CFG = 'yolov5/models/yolov5s.yaml'
         YAML = f'fold{num}.yaml'
         NAME = f'fold{num}'
